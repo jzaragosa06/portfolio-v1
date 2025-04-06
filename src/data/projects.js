31c366aa9afc4328e5fce904f73d50110ddd959c
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-06 22:55:54 +0800

    feat: add About and Contact components; update projects data with images and extended descriptions

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,19 +1,33 @@
 const projectsData = [
     {
         title: "Automated Time Series",
-        description: "lorem ipsum",
+        description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        image: 'https://images.ctfassets.net/hrltx12pl8hq/54UFl7iiibDdhVZwDHKxID/59f5656b5ed34e25ba3e14aa7979f004/0_hero.webp?fit=fill&w=600&h=400',
         tags: ['time series', 'forecasting', 'laravel']
     },
     {
-        title: "Automated Time Series 1",
-        description: "lorem ipsum",
+        title: "Automated Time Series",
+        description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        image: 'https://images.ctfassets.net/hrltx12pl8hq/54UFl7iiibDdhVZwDHKxID/59f5656b5ed34e25ba3e14aa7979f004/0_hero.webp?fit=fill&w=600&h=400',
         tags: ['time series', 'forecasting', 'laravel']
     },
+
     {
-        title: "Automated Time Series 2",
-        description: "lorem ipsum",
+        title: "Automated Time Series",
+        description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        image: 'https://images.ctfassets.net/hrltx12pl8hq/54UFl7iiibDdhVZwDHKxID/59f5656b5ed34e25ba3e14aa7979f004/0_hero.webp?fit=fill&w=600&h=400',
         tags: ['time series', 'forecasting', 'laravel']
-    }
+    },
+
+    {
+        title: "Automated Time Series",
+        description: "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+        image: 'https://images.ctfassets.net/hrltx12pl8hq/54UFl7iiibDdhVZwDHKxID/59f5656b5ed34e25ba3e14aa7979f004/0_hero.webp?fit=fill&w=600&h=400',
+        tags: ['time series', 'forecasting', 'laravel']
+    },
+
+
+
 ];
 
 
