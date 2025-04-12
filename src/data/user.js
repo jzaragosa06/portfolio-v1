45d5741eae9d90f5cec6d5b0ee8e4493ce1b1685
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-12 14:58:09 +0800

    feat: add user data object with personal and professional details

---
+++ src/data/user.js
@@ -0,0 +1,23 @@
+const user = {
+    firstNameShort: "Jun",
+    lastName: "Zaragosa",
+    shortName: "Jun Zaragosa",
+    name: "Jun Jun Zaragosa",
+    email: "junjunzaragosa2309@gmail.com",
+    linkedin: "https://www.linkedin.com/in/jun-jun-zaragosa/",
+    github: "",
+    twitter: "",
+    title: "Software Engineer Intern | Information Technology | DOST Scholar",
+    about: `
+            I am an information technology student major in web and mobile
+            technologies development. In addition to building web systems, I also have a
+            strong passion in data analysis and data science
+            `,
+    skills: ["Node.JS", "Laravel", "Javascript", "Python", "PHP", "React", "Mysql", "PostreSQL", "Machine Learning", "Data Analysis", "Excel", "Jasp", "SCRUM", "Backend-development", "Web-scraping", "Vercel", "Git", "Github"],
+
+
+
+
+}
+
+export default user; 
