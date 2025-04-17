9ff8f39293466ef2316c14b35bf73fb624c6daed
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-18 00:17:58 +0800

    fix: remove homepage and deployment scripts from package.json; update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/portfolio-v1",
   server: {
     port: 3001
   },
