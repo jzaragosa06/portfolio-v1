10ea482847a317e18e41f8cfa16c9ca28adaec12
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-17 16:58:45 +0800

    fix: update deployment directory in package.json and set base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/portfolio-v1",
   server: {
     port: 3001
-  }
+  },
 })
