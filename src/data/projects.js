7224a59fc4553fc146d7a5b7185df1d42e0b86e0
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-04-17 18:05:01 +0800

    fix: update project details with new subtitles, deployment links, and skills learned

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,27 +2,28 @@ const projectsData = [
     {
         id: "1",
         title: "Brainstorm AI",
-        subtitle: "sub",
+        subtitle: "Interactive Node based Idea Explanation/Geneation",
         description: "A web application developed using Node.js.",
         image: "https://www.datascienceportfol.io/static/profile_pics/pr10_522AC6CB762ADACFF4CC.png",
         tags: ['time series', 'forecasting', 'laravel'],
         fullDescription: "A web application developed using Node.js. It features a force-directed graph of wikipedia articles and using Google Gemini Pro-text to relate information extracted from wiki articles.",
-        deployment: "vercel.com",
+        deployment: "https://fdg-and-ai.onrender.com/",
         repository: "https://github.com/jzaragosa06/brainstorm-ai",
-        source: "", 
-        skillsLearned: ['Machine learning', "data processing"],
-        colaborators: [
-            {
-                name: "Colab 1",
-                github: "github.com",
-                email: "sample@gmail.com",
-            },
-            {
-                name: "Colab 1",
-                github: "github.com",
-                email: "sample@gmail.com",
-            }
-        ]
+        source: "https://fdg-and-ai.onrender.com/",
+        skillsLearned: ['D3.JS', 'Node.JS', 'EJS', 'Gemini', 'Machine learning', "Data processing"],
+    },
+    {
+        id: "1",
+        title: "P2P File Sharing",
+        subtitle: "P2P File Sharing Web Application",
+        description: "A web application developed using Node.js.",
+        image: "https://www.datascienceportfol.io/static/profile_pics/pr10_522AC6CB762ADACFF4CC.png",
+        tags: ['Node.JS', 'Websocket', 'socket.io', 'Client-server-architecture'],
+        fullDescription: "A web-based point-to-point file-sharing application that uses WebSocket (Socket.IO) for real-time communication.",
+        deployment: "https://p2p-file-share-awrd.onrender.com/",
+        repository: "https://github.com/jzaragosa06/p2p-file-share",
+        source: "https://github.com/jzaragosa06/p2p-file-share",
+        skillsLearned: ['Node.JS', 'Websocket', 'socket.io', 'Client-server-architecture', "Data processing"],
     },
 ];
 
